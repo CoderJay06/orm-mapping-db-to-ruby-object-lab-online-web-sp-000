334794ea4aad993023ce7a4bdfe5d5be261e2c78
Jason J Cruz <jaycru06@yahoo.com>
2020-02-12 02:11:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,7 +85,7 @@ class Student
     first_x_students
   end 
   
-  def self.first_X_students_in_grade_10
+  def self.first_student_in_grade_10
     sql = <<-SQL 
       SELECT * FROM students 
       WHERE grade = 10 
